083989b47474039eec9642fc48d9ecf23aca51ba
kimhyeonghu <bill4645@gmail.com>
2022-01-05 01:57:59 +0800

    Navigation Bar

--- Controller/app.py
+++ Controller/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../View/HTML', static_folder='../View/static')
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def homepage():
+    return render_template('homepage.html')
 if __name__ == "__main__":
-     b app.run(debug=True)
+     app.run(debug=True)
