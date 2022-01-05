bea55a87bf38c7baca2d5e791b14ebb02eb53c27
kimhyeonghu <bill4645@gmail.com>
2022-01-05 16:08:25 +0800

    Nav bar changes to login status

--- Controller/app.py
+++ Controller/app.py
@@ -17,11 +17,25 @@ storage = firebase.storage()
 app = Flask(__name__, template_folder='../View/HTML', static_folder='../View/static')
 
 user_email = 'test@gmail.com'
-user_password = '1234test'
-auth.sign_in_with_email_and_password(user_email,user_password)
+user_password = '1234ttest'
+login_stat = False
+try:
+    auth.sign_in_with_email_and_password(user_email,user_password)
+    user = auth.current_user
+except:
+    print("Unable to login")
+else:
+    print("User:", user)
 
+if auth.current_user != None:
+    login_stat = True
+    print("Hello")
+else:
+    login_stat = False
+    print("Bye")
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    return render_template('homepage.html',login_stat_html=login_stat)
+
 if __name__ == "__main__":
      app.run(debug=True)
