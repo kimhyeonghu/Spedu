4253bae118da78be0ee9cb4ae7ef5e60cd4b2fee
kimhyeonghu <bill4645@gmail.com>
2022-01-08 03:06:55 +0800

    added sports_course page

--- Controller/app.py
+++ Controller/app.py
@@ -17,7 +17,7 @@ storage = firebase.storage()
 app = Flask(__name__, template_folder='../View/HTML', static_folder='../View/static')
 
 user_email = 'test@gmail.com'
-user_password = '1234ttest'
+user_password = '1234test'
 login_stat = False
 try:
     auth.sign_in_with_email_and_password(user_email,user_password)
@@ -29,13 +29,13 @@ else:
 
 if auth.current_user != None:
     login_stat = True
-    print("Hello")
 else:
     login_stat = False
-    print("Bye")
 @app.route('/')
 def homepage():
     return render_template('homepage.html',login_stat_html=login_stat)
-
+@app.route('/sports_courses/')
+def sports_courses():
+    return render_template('sports_courses.html',login_stat_html=login_stat)
 if __name__ == "__main__":
      app.run(debug=True)
