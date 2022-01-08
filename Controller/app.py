0f64177975f9e8a37b4aee871c0a3c8164743636
Kim Hyeong Hu <bill4645@gmail.com>
2022-01-08 21:31:33 +0800

    Firebase Firestore Connected Successfully

--- Controller/app.py
+++ Controller/app.py
@@ -1,20 +1,38 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 import pyrebase
+
+import firebase_admin
+from firebase_admin import credentials, firestore
 firebaseConfig = {
   "apiKey": "AIzaSyAhONsOYfqp3ox_L5bCy0cpX_f-iQxdc9I",
   "authDomain": "spedu-3fd4f.firebaseapp.com",
   "projectId": "spedu-3fd4f",
   "storageBucket": "spedu-3fd4f.appspot.com",
   "messagingSenderId": "297243626132",
-    "databaseURL":"https://spedu-3fd4f-default-rtdb.asia-southeast1.firebasedatabase.app/",
+"databaseURL":"https://spedu-3fd4f-default-rtdb.asia-southeast1.firebasedatabase.app/",
   "appId": "1:297243626132:web:129e6edfab962e5ab24281"
 }
+
+cred = credentials.Certificate("../serviceAccountKey.json")
+firebase_admin.initialize_app(cred, firebaseConfig)
+
+
+
 firebase = pyrebase.initialize_app(firebaseConfig)
-db = firebase.database()
+db = firestore.client()
 auth = firebase.auth()
 storage = firebase.storage()
 
+
+
+
+
 app = Flask(__name__, template_folder='../View/HTML', static_folder='../View/static')
+
+
+
+
+
 
 user_email = 'test@gmail.com'
 user_password = '1234test'
@@ -24,13 +42,20 @@ try:
     user = auth.current_user
 except:
     print("Unable to login")
-else:
-    print("User:", user)
-
 if auth.current_user != None:
     login_stat = True
 else:
     login_stat = False
+
+
+
+
+
+
+
+
+
+
 @app.route('/')
 def homepage():
     return render_template('homepage.html',login_stat_html=login_stat)
