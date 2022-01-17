f5fb9ff17cd2256f71cfcedba463bddc477b6fed
Nixeld <ethanongchuanming@gmail.com>
2022-01-17 23:56:02 +0800

    Made it look nicer

--- Controller/app.py
+++ Controller/app.py
@@ -20,8 +20,6 @@ db = firestore.client()
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "signin"
-
-
 # login_manager.login_message_category = "info"
 
 # firebaseConfig = {
