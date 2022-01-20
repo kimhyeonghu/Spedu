16f8728a2252f2e884ca31cb05c8dfff7df6a8e6
Kim Hyeong Hu <bill4645@gmail.com>
2022-01-20 13:01:36 +0800

    unified!

--- Model/Product.py
+++ Model/Product.py
@@ -1,8 +1,10 @@
 class Product:
-    def __init__(self, category, image, name, price, reviews, rating,):
+    def __init__(self, productID, category, image, name, price, description, rating, reviews):
+        self.productID = productID
         self.category = category
         self.image = image
         self.name = name
         self.price = price
-        self.reviews = reviews
+        self.description = description
         self.rating = rating
+        self.reviews = reviews
